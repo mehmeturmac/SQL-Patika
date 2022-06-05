1f8118a27c867f3cc3836be12f874fafc42ea244
Mehmet Urmaç <104460988+mehmeturmac@users.noreply.github.com>
2022-06-05 20:48:15 +0300

    Create Odev12.sql
    Alt sorgu kullanımı

---
+++ Odev12.sql
@@ -0,0 +1,37 @@
+-- Patika Ödev Linki: https://app.patika.dev/courses/sql/Odev11
+
+
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- 1.film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film 
+WHERE length > 
+( 
+  SELECT AVG(length) FROM film 
+);
+
+-- 2.film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film 
+WHERE rental_rate = 
+( 
+  SELECT MAX(rental_rate) FROM film 
+);
+
+-- 3.film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film 
+WHERE rental_rate = 
+( 
+  SELECT MIN(rental_rate) FROM film
+) 
+AND replacement_cost = 
+( 
+  SELECT MIN(replacement_cost) FROM film 
+);
+
+-- 4.payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT COUNT(payment_id),
+(SELECT first_name FROM customer WHERE payment.customer_id=customer.customer_id),
+(SELECT last_name FROM customer WHERE payment.customer_id=customer.customer_id)
+FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(payment_id) DESC;
